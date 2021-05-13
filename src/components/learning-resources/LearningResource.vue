18131d9e0e4312f2711b6e4f2be9d68152f1dd05
ascaseiro <ascaseiroweb@hotmail.com>
2021-05-13 15:25:36 +0200

    Learning resources list created in App.vue / Learning resource list item created in LearningResource.vue

---
+++ src/components/learning-resources/LearningResource.vue
@@ -0,0 +1,20 @@
+<template>
+  <li>
+    <div>
+      <header>
+        <h3>{{ title }}</h3>
+        <button>Eliminar</button>
+      </header>
+    </div>
+    <p>{{ description }}</p>
+    <nav>
+      <a :href="link">Ir al recurso</a>
+    </nav>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['title', 'description', 'link']
+}
+</script>
