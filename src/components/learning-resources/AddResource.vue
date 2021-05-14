aa033b5dc160c3bcde4d19c9f51c19492f7c0f09
ascaseiro <ascaseiroweb@hotmail.com>
2021-05-14 16:23:44 +0200

    Data validation in AddResource.vue

--- src/components/learning-resources/AddResource.vue
+++ src/components/learning-resources/AddResource.vue
@@ -1,4 +1,20 @@
 <template>
+  <base-dialog
+    v-if="inputIsInvalid"
+    title="Invalid Input"
+    @close="confirmError"
+  >
+    <template #default>
+      <p>Alguno de los campos parece ser inválido.</p>
+      <p>
+        Por favor, revisa todos los campos y asegúrate de que todos tienen
+        contenido
+      </p>
+    </template>
+    <template #actions>
+      <base-button @click="confirmError">Aceptar</base-button>
+    </template>
+  </base-dialog>
   <base-card>
     <form @submit.prevent="submitData">
       <div class="form-control">
@@ -28,13 +44,30 @@
 <script>
 export default {
   inject: ['addResource'],
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
   methods: {
     submitData() {
       const enteredTitulo = this.$refs.tituloInput.value;
       const enteredDescripcion = this.$refs.descInput.value;
       const enteredLink = this.$refs.linkInput.value;
 
+      if (
+        enteredTitulo.trim() === '' ||
+        enteredDescripcion.trim() === '' ||
+        enteredLink.trim() === ''
+      ) {
+        this.inputIsInvalid = true;
+        return;
+      }
+
       this.addResource(enteredTitulo, enteredDescripcion, enteredLink);
+    },
+    confirmError() {
+      this.inputIsInvalid = false;
     },
   },
 };
